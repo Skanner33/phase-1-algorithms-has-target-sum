e7c5c91d032c3470657c79d2ff69872555afdbd2
Skanner33 <mathewmageto@gmail.com>
2022-12-09 14:07:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -14,14 +14,30 @@ function hasTargetSum(array, target) {
 
 /* 
   Write the Big O time complexity of your function here
+  time complexity = O(n*2)
 */
 
 /* 
   Add your pseudocode here
+   for (let i = 0; i < array.length; i++){
+    const targetSum = target - array [i];
+
+   for (let k = i + 1; k < array.length; k++){
+    if (array [k] === targetSum) return true;
+
+  } 
+
+  }
+   return false; 
+}
 */
 
 /*
   Add written explanation of your solution here
+  We'll let our target minus the arrays provided and check whether the result can complement our array when added to provide the target
+  Our initializer will be targetSum (target - array[i])
+  We'll use the following approach to check whether the targetSum can be added to the array
+  If targetSum can be added to the array, then we'll return true
 */
 
 // You can run `node index.js` to view these console logs
